39e31859af3d7d21b4a553e9718b226ad1b67830
Mihir Kadiya <mihir@Mihirs-MacBook-Pro.local>
2022-08-24 18:44:36 -0400

    added click & hold functionality on hexagons

--- src/components/Hexgrid.tsx
+++ src/components/Hexgrid.tsx
@@ -14,6 +14,7 @@ export default function Hexgrid(this: any, props: any) {
     const hexagons: Hex[] = generator.apply(this, config.mapProps);
 
     let [type, setType] = useState<State>({hexagons, config});
+    let [mouseHold, setMouseHold] = useState(false);
 
     const changeType = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const map = event.currentTarget.value as keyof typeof configs;
@@ -23,13 +24,26 @@ export default function Hexgrid(this: any, props: any) {
         setType({hexagons, config});
     }
 
-    const handleClick = (event: React.MouseEvent<SVGGElement, MouseEvent>, hexID: string) => {
+    const onClick = (event: React.MouseEvent<SVGGElement, MouseEvent>, hexID: string) => {
         console.log(hexID);
-        const coloredHexes = type.hexagons.map(hex => {
-          hex.props = hex.props || {};
-          // hex.props.className = 'bg-white';
-          return hex;
-        })
+        console.log(event.currentTarget);
+    }
+
+    const onMouseDown = (event: React.MouseEvent<SVGGElement, MouseEvent>) => {
+      setMouseHold(true);
+      const hexagon = event.currentTarget;
+      hexagon.style.fill = 'black'
+    }
+
+    const onMouseUp = (event: React.MouseEvent<SVGGElement, MouseEvent>) => {
+      setMouseHold(false);
+    }
+
+    const onMouseEnter = (event: React.MouseEvent<SVGGElement, MouseEvent>) => {
+      const hexagon = event.currentTarget;
+      if (mouseHold) {
+        hexagon.style.fill = 'black'
+      }
     }
 
     useEffect(() => {
@@ -40,12 +54,6 @@ export default function Hexgrid(this: any, props: any) {
     const state = type;
     const layout = state.config.layout;
     const size = { x: layout.width, y: layout.height };
-    const hexStyle = {
-        fill: 'white',
-        stroke: 'black',
-        'stroke-linejoin': 'round',
-        'stroke-width': '0.1px'
-    };
     return (
       <div className="App">
         <h2>Select grid type and configuration from dropdown.</h2>
@@ -64,8 +72,14 @@ export default function Hexgrid(this: any, props: any) {
               // note: key must be unique between re-renders.
               // using config.mapProps+i makes a new key when the goal template chnages.
               state.hexagons.map((hex, i) => (
-                <Hexagon style={hexStyle} key={state.config.mapProps.toString() + i} q={hex.q} r={hex.r} s={hex.s}
-                    onClick={(event) => handleClick(event, HexUtils.getID(hex))}>
+                <Hexagon 
+                    className='fill-white stroke-1 stroke-black hover:fill-slate-300'
+                    key={state.config.mapProps.toString() + i}
+                    q={hex.q} r={hex.r} s={hex.s}
+                    onClick={(event) => onClick(event, HexUtils.getID(hex))}
+                    onMouseDown={(e) => onMouseDown(e)}
+                    onMouseUp={(e) => onMouseUp(e)}
+                    onMouseEnter={(e) => onMouseEnter(e)}>
                 </Hexagon>
               ))
             }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      strokeWidth: {
+        '1': '0.1px'
+      }
+    },
   },
   plugins: [],
 }
